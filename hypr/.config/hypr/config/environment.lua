241265df1c4050eedce3c636180ef3f5986db045
Vyseriann <chase@karlsensg.net>
2026-08-02 18:53:07 +0800

    update configuration files

--- hypr/.config/hypr/config/environment.lua
+++ hypr/.config/hypr/config/environment.lua
@@ -11,7 +11,7 @@ hl.config({
 		border_size = 1,
 
 		col = {
-			active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
+			active_border = { colors = { "#B0924D" }, angle = 45 },
 			inactive_border = "rgba(595959aa)",
 		},
 
